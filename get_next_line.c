fc6a7d1f41abbde8cfd5f43ad8d17b743d5d3113
Segolene ALQUIER <salquier@e1r8p6.42.fr>
2018-11-26 19:18:57 +0100

    no working yet - take a look at realloc girl

--- get_next_line.c
+++ get_next_line.c
@@ -6,37 +6,125 @@
 /*   By: salquier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/25 16:40:50 by salquier          #+#    #+#             */
-/*   Updated: 2018/11/26 13:32:29 by salquier         ###   ########.fr       */
+/*   Updated: 2018/11/26 19:03:48 by salquier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
+# include "../libft/libft.h"
 #include "get_next_line.h"
 #include <stdio.h>
 
-int		get_next_line(const int fd, char **line)
+/*int		get_next_line(const int fd, char **line)
 {
-	char buf[BUFF_SIZE];
+	char buf[BUFF_SIZE + 1];
 	int i;
-	
+	int newline;
+	int letter;
+
 	i = 0;
 	line = NULL;
+	newline = 0;
 	if (fd == -1)
 		return (1);
-	while (read(fd, buf+i, 1))
+	while (buf[i] != EOF)
 	{
-			printf("line[0][%d] = %c\n", i, line[0][i]);
-		if (buf[i] != '\n')
+		letter = 0;
+		line = (char **)malloc(sizeof(char *) * (newline + 1));
+		while (read(fd, buf+i, 1) && buf[i] != '\n')
 		{
-			line[0][i] = buf[i];
+			line[newline] = (char *)malloc(sizeof(char) * (letter + 1));
+			line[newline][letter] = buf[i];
 			printf("buf[%d] = %c\n", i, buf[i]);
-			printf("line[0][%d] = %c\n", i, line[0][i]);
+			printf("newline[%d][%d] = %c\n", newline, letter, line[newline][letter]);
 			i++;
+			letter++;
 		}
-		else
-			return (123);
+		i++;
+		newline++;
 	}
 	buf[i] = 0;
 	//write(1, buf, size);
+	return (0);
+}*/
+/*int		get_next_line(const int fd, char **line)
+{
+	static char buf[BUFF_SIZE + 1];
+	static int i;
+	int newline;
+	int letter;
+	char *tmp;
+
+	i = 0;
+	//line = NULL;
+	newline = 0;
+	if (fd == -1)
+		return (1);
+	while (read(fd, buf+i, BUFF_SIZE))
+	{
+		printf("buf[%d] = %c\n", i, buf[i]);
+		letter = 0;
+		line = (char **)malloc(sizeof(char *) * (newline + 1));
+		while (buf[i] != '\n')
+		{
+			line[newline] = (char *)malloc(sizeof(char) * (letter + 1));
+			line[newline][letter] = buf[i];
+			printf("buf[%d] = %c\n", i, buf[i]);
+			printf("newline[%d][%d] = %c\n", newline, letter, line[newline][letter]);
+			i++;
+			letter++;
+		}
+		i++;
+		printf("i = %d\n", i);
+		newline++;
+	}
+	line[newline] = 0;
+	return (0);
+}*/
+int		get_next_line(const int fd, char **line)
+{
+	static char buf[BUFF_SIZE + 1];
+	static int i;
+	//int newline;
+	//int letter;
+	char *tmp;
+	size_t size;
+
+	i = 0;
+	line = NULL;
+	//newline = 0;
+	if (fd == -1)
+		return (1);
+	// GET NB OF BYTES IN FILE VIA READ //
+	tmp = ft_strnew(size = read(fd, buf+i, BUFF_SIZE));
+	// ALLOCATE SUFFICIENT MEMORY //
+	//tmp = ft_strnew(size);
+	// COPY TEXT IN BUFFER //
+	printf("size = %lu\n", size);
+	printf("tmp[74] = %c\n", tmp[77]);
+	i = 0;
+	while (read(fd, buf+i, BUFF_SIZE))
+	{
+		tmp[i] = buf[i];
+		printf("buf[%d] = %c\n", i, buf[i]);
+		printf("tmp[%d] = %c\n", i, tmp[i]);
+
+		i++;
+	}
+		/*letter = 0;
+		line = (char **)malloc(sizeof(char *) * (newline + 1));
+		while (buf[i] != '\n')
+		{
+			line[newline] = (char *)malloc(sizeof(char) * (letter + 1));
+			line[newline][letter] = buf[i];
+			printf("buf[%d] = %c\n", i, buf[i]);
+			printf("newline[%d][%d] = %c\n", newline, letter, line[newline][letter]);
+			i++;
+			letter++;
+		}
+		i++;
+		printf("i = %d\n", i);
+		newline++;
+	}
+	line[newline] = 0;*/
 	return (0);
 }
 
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: salquier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/25 16:44:20 by salquier          #+#    #+#             */
-/*   Updated: 2018/11/26 11:57:13 by salquier         ###   ########.fr       */
+/*   Updated: 2018/11/26 17:01:15 by salquier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define GET_NEXT_LINE_H
 # define BUFF_SIZE 300
 # include <unistd.h>
+# include <stdlib.h>
 # include <sys/types.h>
 # include <sys/stat.h>
 # include <fcntl.h>
