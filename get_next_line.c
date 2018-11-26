1f731edd0be1896075265f46be6392a8313170f2
Segolene ALQUIER <salquier@e1r8p5.42.fr>
2018-11-26 15:08:39 +0100

    added all to gnl

--- get_next_line.c
+++ get_next_line.c
@@ -6,26 +6,48 @@
 /*   By: salquier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/25 16:40:50 by salquier          #+#    #+#             */
-/*   Updated: 2018/11/25 16:59:29 by salquier         ###   ########.fr       */
+/*   Updated: 2018/11/26 13:32:29 by salquier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdio.h>
 
 int		get_next_line(const int fd, char **line)
 {
+	char buf[BUFF_SIZE];
+	int		size;
+	int i;
 
-
+	i = 0;
+	size = BUFF_SIZE;
+	line = NULL;
+	if (fd == -1)
+		return (1);
+	while (i < size)
+	{
+		if (buf[i] != '\n')
+		{
+			size = read(fd, buf+i, 1);
+			printf("buf[%d] = %c\n", i, buf[i]);
+			i++;
+		}
+		else
+			return (123);
+	}
+	buf[size] = 0;
+	write(1, buf, size);
+	return (0);
 }
 
 
 int		main(void)
 {
 	int fd;
-	char buf[BUFF_SIZE];
-	int size;
-
+	char **line;
+	line = NULL;
 	fd = open("test", O_RDONLY);
-	if (fd == -1)
-		return (1);
+	printf("%d\n", get_next_line(fd, line));
+	close(fd);
+	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,17 @@
 /*   By: salquier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/25 16:44:20 by salquier          #+#    #+#             */
-/*   Updated: 2018/11/25 16:55:50 by salquier         ###   ########.fr       */
+/*   Updated: 2018/11/26 11:57:13 by salquier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 32
+# define BUFF_SIZE 300
 # include <unistd.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
 
 int		get_next_line(const int fd, char **line);
 
